9a5b162a87f7b678b4a841346d28b322358a52dc
martincorneliuspotgieter <martin.cornelius.potgieter@kegumaskola.lv>
2026-05-16 18:38:10 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+// Initialize the KAPLAY engine instance.
+        // This function automatically creates an HTML5 <canvas> element and injects it into the body.
+        kaplay({
+            background: [135, 206, 235], 
+            width: window.innerWidth,
+            height: window.innerHeight,
+            // Keeps your game aspect ratio consistent if the window is resized (prevents stretching distortion)
+            letterbox: true, 
+        });
